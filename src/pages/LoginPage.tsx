40b9bbc35fb9e7056d81bb8316e8c31fd499c09f
rafram96 <rafael.ramos@utec.edu.pe>
2024-11-09 17:08:50 -0500

    Login Page Check

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -8,18 +8,9 @@ export default function LoginPage() {
 				<Button message="Iniciar Sesión" to="/auth/login" />
 				<Button message="Registrarse" to="/auth/register" />
 			</section>
-
-			<article className="flex justify-between">
-				<LoginForm />
-				<section className="login-section flex flex-col items-center p-4 text-center">
-					<h2 className="title">Bienvenido de vuelta</h2>
-					<p>Inicia sesión para empezar a usar Uber</p>
-				</section>
-				<section className="w-1/2 flex flex-col items-center p-4 text-center">
-					<h2 className="text-2xl font-semibold mb-2">Bienvenido de vuelta</h2>
-					<p className="text-gray-600 mb-4">Inicia sesión para empezar a usar Uber</p>
-				</section>
-			</article>
+      <article className="flex justify-center items-center py-8">
+        <LoginForm />
+      </article>
 		</main>
 	);
 }
