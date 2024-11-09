43e75050446d1e1ed8cd59bc6a6afd59f2030a53
rafram96 <rafael.ramos@utec.edu.pe>
2024-11-09 16:43:31 -0500

    Routing Login Check

---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,28 @@
+import img4 from "@assets/Img4.jpg";
+import Button from "../components/Button";
+import LoginForm from "../components/LoginForm";
+
+export default function LoginPage() {
+	return (
+		<main className="px-10">
+			<section className="flex justify-center items-center py-4">
+				<Button message="Iniciar Sesión" to="/auth/login" />
+				<Button message="Registrarse" to="/auth/register" />
+			</section>
+
+			<article className="flex justify-between">
+				<LoginForm />
+				<section className="login-section flex flex-col items-center p-4 text-center">
+					<h2 className="title">Bienvenido de vuelta</h2>
+					<p>Inicia sesión para empezar a usar Uber</p>
+					<img src={img4} alt="uber" />
+				</section>
+				<section className="w-1/2 flex flex-col items-center p-4 text-center">
+					<h2 className="text-2xl font-semibold mb-2">Bienvenido de vuelta</h2>
+					<p className="text-gray-600 mb-4">Inicia sesión para empezar a usar Uber</p>
+					<img src={img4} alt="uber" className="w-3/4" />
+				</section>
+			</article>
+		</main>
+	);
+}
